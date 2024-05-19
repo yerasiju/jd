3c9dce65cb2f4ca764b312effa42333edb3f519f
yerasiju <erkebulan.zholymbek@bk.ru>
2024-05-20 00:05:54 +0500

    dsd

--- lib/main.dart
+++ lib/main.dart
@@ -7,109 +7,103 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context){
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.lightBlue[50], // Added background color
         appBar: AppBar(
           title: Text(
             'Weather',
-            style: TextStyle(color:Colors.black87),
+            style: TextStyle(color: Colors.black87),
           ),
           centerTitle: true,
           backgroundColor: Colors.white,
-          leading: IconButton(icon:Icon(Icons.menu), onPressed: () {}),
+          leading: IconButton(icon: Icon(Icons.menu), onPressed: () {}),
           iconTheme: IconThemeData(color: Colors.black54),
           actions: <Widget>[
-            IconButton(icon:Icon(Icons.settings), onPressed: () {}),
+            IconButton(icon: Icon(Icons.settings), onPressed: () {}),
           ],
         ),
-        body: _buildBody(),
+        body: SingleChildScrollView(
+          child: Column(
+            children: <Widget> [
+              _headerImage(),
+              SafeArea(
+                child: Padding(
+                  padding: EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      _weatherDescription(),
+                      Divider(),
+                      _temperature(),
+                      Divider(),
+                      _temperatureForecast(),
+                      Divider(),
+                      _footerRatings(),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
 
-Widget _buildBody(){
-  return SingleChildScrollView(
-    child: Column(
-      children: <Widget> [
-        _headerImage(),
-        SafeArea(child: Padding(
-          padding: EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children:<Widget> [
-              _weatherDescription(),
-              Divider(),
-              Divider(),
-              _temperature(),
-              Divider(),
-              _temperatureForecast(),
-              Divider(),
-              _footerRatings(),
-            ],
-          ),
-        ),
-        ),
-      ],
-
-  ),
-  );
-}
 Image _headerImage(){
   return Image(
-      image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaZ2_esmFn5-78Pq2YUCcQZjNkh9CHRT06Qw&s'),
+    image: NetworkImage('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaZ2_esmFn5-78Pq2YUCcQZjNkh9CHRT06Qw&s'),
     fit: BoxFit.cover,
   );
-
 }
 
 Column _weatherDescription(){
   return Column(
     crossAxisAlignment: CrossAxisAlignment.center,
-    children:<Widget> [
-      Text('Tuesday - June 17',
-      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30)
+    children: <Widget>[
+      Text(
+        'Tuesday - June 17',
+        style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 30),
       ),
       Divider(),
-      Text('Lorem iansdnsanljs jnljdnfljd fndfbkhafkabdhbadn a acbjdbjbkjdabn ds bdsjkbjfdsabjlnljnfnjldkj f jjncljnacndnlsna  ewnefljanfjn sdjjjjjjjjjdfk.ewlna', style: TextStyle(color:Colors.black54)),
-
+      Text(
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque malesuada quam at tellus ornare, et vehicula arcu fringilla.',
+        style: TextStyle(color: Colors.black54),
+        textAlign: TextAlign.center, // Added text alignment for better layout
+      ),
     ],
   );
 }
+
 Row _temperature() {
   return Row(
-    mainAxisAlignment:MainAxisAlignment.center ,
+    mainAxisAlignment: MainAxisAlignment.center,
     children: <Widget>[
+      Icon(
+        Icons.wb_sunny_sharp,
+        color: Colors.yellow,
+      ),
+      SizedBox(width: 16.0),
       Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
-          Icon(Icons.wb_sunny_sharp,
-            color: Colors.yellow,
+          Row(
+            children: <Widget>[
+              Text(
+                '15° Clear',
+                style: TextStyle(color: Colors.deepPurple),
+              ),
+            ],
           ),
-
-
+          Row(
+            children: <Widget>[
+              Text(
+                'Jambyl Region, Taraz',
+                style: TextStyle(color: Colors.grey),
+              ),
+            ],
+          ),
         ],
-      ),
-        SizedBox(width: 16.0),
-        Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              children: <Widget>[
-                Text('15* Clear',
-                  style: TextStyle(
-                      color: Colors.deepPurple)
-                  ,)
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Text('Jambyl Region, Taraz',
-                    style: TextStyle(
-                        color: Colors.grey)
-                ),
-              ],
-            ),
-          ],
-
       ),
     ],
   );
@@ -121,10 +115,10 @@ Wrap _temperatureForecast() {
     children: List.generate(8, (int index) {
       return Chip(
         label: Text(
-          '${index + 20} ;C',
+          '${index + 20}°C',
           style: TextStyle(fontSize: 15.0),
         ),
-        avatar: Icon (
+        avatar: Icon(
           Icons.wb_cloudy_rounded,
           color: Colors.blue.shade300,
         ),
@@ -141,12 +135,12 @@ Wrap _temperatureForecast() {
 Row _footerRatings() {
   var stars = Row(
     mainAxisSize: MainAxisSize.min,
-    children:<Widget> [
-      Icon(Icons.star, size: 15, color: Colors.yellow[700],),
-      Icon(Icons.star, size: 15, color: Colors.yellow[700],),
-      Icon(Icons.star, size: 15, color: Colors.yellow[700],),
-      Icon(Icons.star, size: 15, color: Colors.yellow[700],),
-      Icon(Icons.star, size: 15, color: Colors.grey[500],)
+    children: <Widget>[
+      Icon(Icons.star, size: 15, color: Colors.yellow[700]),
+      Icon(Icons.star, size: 15, color: Colors.yellow[700]),
+      Icon(Icons.star, size: 15, color: Colors.yellow[700]),
+      Icon(Icons.star, size: 15, color: Colors.yellow[700]),
+      Icon(Icons.star_half, size: 15, color: Colors.yellow[700]),
     ],
   );
   return Row(
@@ -155,10 +149,8 @@ Row _footerRatings() {
       Text(
         'Info with openweathermap.org',
         style: TextStyle(fontSize: 15),
-
       ),
       stars,
     ],
-
   );
 }
